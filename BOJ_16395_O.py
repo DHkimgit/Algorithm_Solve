aee1749524912fefda351c69c6c76c4fc2292450
DHkimgit <dukekim001@naver.com>
2022-05-29 05:43:02 +0000

    2205291442

---
+++ BOJ_16395_O.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+
+      
+def factorial(k):
+    l = 1
+    for i in range(2, k+1):
+        l *= i
+    return l
+
+def solve(n, k):
+    ans = factorial(n) // factorial(k) // factorial(n-k)
+    return ans
+
+print(solve(n-1, k-1))
