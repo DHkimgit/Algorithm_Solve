516f85cdafb541c441ce5f560dc57a216f94307d
DHkimgit <dukekim001@naver.com>
2022-06-01 04:20:28 +0000

    06011320

---
+++ Dynamic_Programming/BOJ_1912_X_brute force_TIMEOUT.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+
+int_sum = 0
+sum = 0
+
+for start in range(n):
+    for end in range(start + 1, n):
+        int_sum = 0
+        for i in range(start, end):
+            int_sum = int_sum + arr[i]
+        if int_sum > sum:
+            sum = int_sum
+        else:
+            continue            
+            
+print(sum)
