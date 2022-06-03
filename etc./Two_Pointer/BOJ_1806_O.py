137931ced2b24986bc10a858be09a92a03b3ccb3
DHkimgit <dukekim001@naver.com>
2022-06-03 10:08:49 +0000

    06031908

--- etc./Two_Pointer/BOJ_1806_O.py
+++ etc./Two_Pointer/BOJ_1806_O.py
@@ -4,7 +4,6 @@ arr = list(map(int, input().split()))
 end = 0
 interval_sum = 0
 ls = []
-count = 0
 sum_arr = 0
 
 for i in range(N):
@@ -13,15 +12,23 @@ for i in range(N):
 if sum_arr < S:
     print(0)
 
+elif S == 0:
+    print(1)
+
 else :
     for start in range(N):
+        if interval_sum >= S:
+                ls.append(end - start) 
         while interval_sum < S and end < N:
             interval_sum = interval_sum + arr[end]
+            if interval_sum >= S:
+                ls.append(end - start + 1)
             end = end + 1
-        if interval_sum >= S:
-            count += 1
-            ls.append(end - start + 1)
-    
         interval_sum = interval_sum - arr[start]
     
     print(min(ls))
+"""
+N = 10, S = 100일때 배열이 98 1 1 1 50 50 1 1 1 98 인 경우 중간 start = 4, end = 5 일때 최솟값이 나와야 한다. 따라서 2가 답이어야 하는데
+
+
+"""
